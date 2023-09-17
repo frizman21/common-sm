df089daca0ba34ad31b25ed79642b0a35f16f156
Mike Frizzell <mfrizzell@thealbersgroup.com>
2023-09-16 19:38:10 -0500

    corrects code formatting issues

--- src/main/java/io/github/frizman21/common/sm/CancelTimeoutActivity.java
+++ src/main/java/io/github/frizman21/common/sm/CancelTimeoutActivity.java
@@ -1,21 +1,25 @@
 package io.github.frizman21.common.sm;
 
+/**
+ * This activity cancels a previous Timeout Activity that may be running.
+ */
 public class CancelTimeoutActivity extends AbstractActivity {
 
-	TimeoutActivity timeoutActivity;
-	
-	public CancelTimeoutActivity(TimeoutActivity timeoutActivity) {
-		super();
-		this.timeoutActivity = timeoutActivity;
-	}
+  TimeoutActivity timeoutActivity;
 
-	@Override
-	public String getName() {
-		return "CancelTimeoutActivity";
-	}
+  public CancelTimeoutActivity(TimeoutActivity timeoutActivity) {
+    super();
+    this.timeoutActivity = timeoutActivity;
+  }
 
-	@Override
-	public void run() {
-		timeoutActivity.cancelTimeout();
-	}
+  @Override
+  public String getName() {
+    return "CancelTimeoutActivity";
+  }
+
+  @Override
+  public void run() {
+    timeoutActivity.cancelTimeout();
+  }
+  
 }
